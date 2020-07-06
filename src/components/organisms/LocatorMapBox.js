6c27e58ecb842cf002072d065e8cf5769d544015
Arik Yavilevich <arik@yavilevich.com>
2020-07-06 10:48:16 +0300

    fit data in view for mapbox

--- src/components/organisms/LocatorMapBox.js
+++ src/components/organisms/LocatorMapBox.js
@@ -3,14 +3,21 @@ import PropTypes from 'prop-types';
 import ReactMapboxGl, {
 	Layer,
 	Feature,
+	MapContext,
 	/*
 	ScaleControl,
 	ZoomControl,
 	RotationControl,
 	*/
 } from 'react-mapbox-gl';
+import * as MapboxGl from 'mapbox-gl';
 import { MapValidPointsPropType } from '../../lib/PropTypes';
 import GLOBAL_CONSTANTS from '../../lib/Constants';
+
+const CONSTANTS = {
+	FIT_MAX_ZOOM: 30,
+	FIT_PADDING_RATIO: 0.1, // 10% on each side
+};
 
 const linePaint = {
 	'line-color': '#4790E5',
@@ -31,12 +38,44 @@ const targetCirclePaint = {
 	'circle-opacity': 0.8,
 };
 
+/*
+Fit all route points in view, with the "center" as the center of the view.
+Fit in such a way that the points will be in view at any rotation/bearing/heading
+Reference: https://github.com/mapbox/mapbox-gl-js/issues/1338
+*/
+function circularFitBounds(map, center, route) {
+	const tr = map.transform;
+	// console.log('fitBounds, tr, ', tr.width, tr.height); // size of the view in pixels
+	const centerTransform = tr.project(MapboxGl.LngLat.convert(center)); // position of center in pixels
+	// console.log('fitBounds, center, ', center, centerTransform);
+	let maxDist = 0;
+	route.forEach((point) => {
+		const transformed = tr.project(MapboxGl.LngLat.convert(point));
+		const dist = transformed.dist(centerTransform);
+		maxDist = Math.max(maxDist, dist);
+		// console.log('fitBounds, point, ', point, transformed, dist);
+	});
+	if (maxDist === 0) {
+		return GLOBAL_CONSTANTS.DEFAULT_MAP_ZOOM;
+	}
+	// return zoom level that will fit a circle at radius of maxDist
+	const lateralPadding = tr.width * CONSTANTS.FIT_PADDING_RATIO;
+	const verticalPadding = tr.height * CONSTANTS.FIT_PADDING_RATIO;
+	const scaleX = (tr.width - lateralPadding * 2) / (maxDist * 2);
+	const scaleY = (tr.height - verticalPadding * 2) / (maxDist * 2);
+	if (scaleY < 0 || scaleX < 0) {
+		console.warn('Map cannot fit within canvas with the given bounds, padding, and/or offset.');
+		return GLOBAL_CONSTANTS.DEFAULT_MAP_ZOOM;
+	}
+	const maxZoom = CONSTANTS.FIT_MAX_ZOOM;
+	return Math.min(tr.scaleZoom(tr.scale * Math.min(scaleX, scaleY)), maxZoom);
+}
+
 class LocatorMapBox extends React.Component {
-	/*
 	constructor(props) {
 		super(props);
+		this.map = null;
 	}
-	*/
 
 	componentDidMount() {
 		const { accessToken } = this.props;
@@ -73,15 +112,6 @@ class LocatorMapBox extends React.Component {
 		const route = points.map((point) => [point.longitude, point.latitude]);
 		// console.log('mapbox render, points', points.length, points, route);
 
-		// figure out optimal bounds, TODO
-		// fitBounds seems to be reseting the bearing (per documentation)
-		// https://docs.mapbox.com/mapbox-gl-js/api/map/#map#fitbounds
-		// https://stackoverflow.com/questions/35586360/mapbox-gl-js-getbounds-fitbounds
-		// example of a custom fitBounds
-		// https://github.com/mapbox/mapbox-gl-js/issues/1338
-		// I might need to do my own to fit a circle rather than a rectangular bounds
-		// radius of the circle based on the route points farthest from the center
-
 		// decide on center of map
 		let center;
 		if (longitude !== false && latitude !== false) {
@@ -92,6 +122,17 @@ class LocatorMapBox extends React.Component {
 			center = [GLOBAL_CONSTANTS.DEFAULT_MAP_CENTER.LONGITUDE, GLOBAL_CONSTANTS.DEFAULT_MAP_CENTER.LATITUDE]; // some default position that is not just ocean
 		}
 		// console.log('map center', center);
+		// set default zoom (before fitting a bounding view)
+		let zoom = GLOBAL_CONSTANTS.DEFAULT_MAP_ZOOM;
+
+		// figure out optimal bounds
+		// fitBounds seems to be reseting the bearing (per documentation)
+		// https://docs.mapbox.com/mapbox-gl-js/api/map/#map#fitbounds
+		// https://stackoverflow.com/questions/35586360/mapbox-gl-js-getbounds-fitbounds
+		// console.log('map in render', this.map);
+		if (this.map && route.length > 0) {
+			zoom = circularFitBounds(this.map, center, route);
+		}
 
 		return (
 			<>
@@ -108,8 +149,15 @@ class LocatorMapBox extends React.Component {
 						}}
 						center={center}
 						bearing={[heading || 0]}
-						zoom={[GLOBAL_CONSTANTS.DEFAULT_MAP_ZOOM]}
+						zoom={[zoom]}
 					>
+						<MapContext.Consumer>
+							{(map) => {
+								// use `map` here
+								// console.log('map in context', map, map.transform);
+								this.map = map; // store map object
+							}}
+						</MapContext.Consumer>
 						{/*
 						<ScaleControl />
 						<ZoomControl />
